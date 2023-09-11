471703bb189c8e8ca450f1fac1ce914cb124ec17
Min Dator <Min Dator@DESKTOP-3DRICQ3>
2023-09-11 09:58:12 +0200

    Add project files.

---
+++ C#Enum/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace C_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enum:
+            // 
+            Console.WriteLine("Hello, Viktor!");
+        }
+    }
+}
